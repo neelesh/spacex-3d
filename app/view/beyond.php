14bee12a0aaccebda7809e1ad4598c84dc046073
Neelesh <neelesh.dooraree@googlemail.com>
2019-05-07 23:07:26 +0100

    Making the Beyond page read well.

--- app/view/beyond.php
+++ app/view/beyond.php
@@ -3,13 +3,13 @@
     <div class="col-sm-12">
         <br>
         <h1 class="heading"><span >GOING BEYOND</span></h1>
-        <div class="jumbotron">
+        <div class="jumbotron" style="font-size:18px;">
             <p class="p1"><span class="s1">This is a web application capable of viewing and interacting with 3D Models. I have chosen to use SpaceX Rockets as the theme. This application was created using X3D with a PHP MVC. This is also a Single Page Application where the content is data-driven using SQLite. I have taken additional steps in creating this application that is outside the course material.</span></p>
             <p class="p1"><span class="s1">The code for this project can be seen on my <a href="https://github.com/neelesh/spacex-3d">Github</a>.</span></p>
             <br>
             
             <h3 class="p1"><span class="s1">X3D</span></h3>
-            <p class="p1"><span class="s1">X3D My models are complex but are still at a suitable polygon count for rendering into a web browser. I have chosen to create SpaceX rockets. I started modeling low poly, blocking out the rockets, with the intention to add additional detail later on. The silver rockets mimic the stainless steel design using a glossy material I made in 3DS Max. Starhopper and Starship have a texture each, the American flag.</span></p>
+            <p class="p1"><span class="s1">X3D My models are complex but are still at a suitable polygon count for rendering into a web browser. I have chosen to create SpaceX rockets. I started modeling low poly, blocking out the rockets, with the intention to add additional detail later on. The silver rockets mimic the stainless steel design using a glossy material I made in 3DS Max. Starhopper and Starship have a texture each, which is of the American flag.</span></p>
             <img class="d-block w-100" src="img/beyond/wireframes.png">
             
             <br>
@@ -34,7 +34,7 @@
             <br>
            
             <h4 class="p1"><span class="s1">Controller for SPA</span></h4>
-            <p class="p1"><span class="s1">Instead of calling the controller for every page, I load the entire site at once. This is done but loading in multiple views, and providing them with the data they require. This also means I only have to retrieve from the database once. Once loaded, the navigation.js file will show and hide the content as required. This method reduces server and database load and makes the site very responsive.</span></p>
+            <p class="p1"><span class="s1">Instead of calling the controller for every page, I load the entire site at once. This is done but loading in multiple views, and providing them with the data they require. This also means I only have to retrieve from the database once. Once loaded, the javascript will show and hide the content as required. This method reduces server and database load and makes the site very responsive.</span></p>
             <img class="d-block w-100" src="img/beyond/controller.png">
             
 
@@ -50,12 +50,15 @@
             <br>
             <br>
             <h3 class="p1"><span class="s1">Home Page Renders</span></h3>
-            <p class="p1"><span class="s1">It appears as though the assignment would accept photographs of the real objects. Half of these rockets don't actually exist yet, and won't until the 2020's. I have opted to render out my rockets and photoshop them into space scenes. I have created the Starhopper in orbit of the earth, the Starship exploring space, the Booster detaching from the starship, and the Falcon 9 returning to earth. I colour corrected the renders in order to fit convincingly in the compositions.&nbsp;</span></p>
+            <p class="p1"><span class="s1">It appears as though the assignment would accept photographs of the real objects. Half of these rockets don't actually exist yet, and won't until the late 2020's. I have chosen to render my rockets and photoshop them into outer space. I have created the Starhopper in orbit of the earth, the Starship exploring space, the Booster detaching from the starship, and the Falcon 9 returning to earth. I colour corrected the renders in order to fit convincingly in the compositions.&nbsp;</span></p>
+            
             <br>
-
-            <img class="d-block w-100" src="img/slides/<?php echo $data['starhopper']['fileName'].= '.jpg';?>" >
+            <img class="d-block w-100" src="img/slides/<?php echo $data['starhopper']['fileName'].= '.jpg';?>" > 
+            <br>
             <img class="d-block w-100" src="img/slides/<?php echo $data['starship']['fileName'].= '.jpg';?>" >
+            <br>
             <img class="d-block w-100" src="img/slides/<?php echo $data['booster']['fileName'].= '.jpg';?>" >
+            <br>
             <img class="d-block w-100" src="img/slides/<?php echo $data['falcon9']['fileName'].= '.jpg';?>" >
             
         </div>
